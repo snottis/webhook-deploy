6e0551d18a1601466f359313d01daf5afa5fdcdc
Sauli Nevalainen <nevalainen.sauli@gmail.com>
2021-10-25 03:04:09 +0300

    Add waiting to commands

--- deploy.js
+++ deploy.js
@@ -1,14 +1,14 @@
 const { exec } = require("child_process");
 const config = require('./config');
 
-const execute = (command) => {
+const execute = async (command) => {
     exec(command, (error, stdout, stderr) => {
         if(error)
             console.log('error:', error)
         if(stdout) 
             console.log('stdout:', stdout)
         if(stderr)
-            console.log('stderr', stderr)
+            console.log('stderr:', stderr)
     })
 }
 
@@ -16,15 +16,15 @@ const listComposeFiles = () => {
     execute('ls -la compose/')
 }
 
-const getDevelopmentFiles = (repoUrl, tagRef) => {
-    execute('rm -rf ./compose/src');
-    execute(`git clone ${repoUrl} ./compose/src`);
-    execute(`cd ./compose/src && git fetch --all && git checkout tags/${tagRef} && cd ../..`);
+const getDevelopmentFiles = async (repoUrl, tagRef) => {
+    await execute('rm -rf ./compose/src');
+    await execute(`git clone ${repoUrl} ./compose/src`);
+    await execute(`cd ./compose/src && git fetch --all && git checkout tags/${tagRef} && cd ../..`);
 }
 
-const getReleaseFiles = (tarUrl) => {
-    execute('rm -rf ./compose/src');
-    execute(`curl ${tarUrl} | tar -xvz - -C ./compose/src`)
+const getReleaseFiles = async (tarUrl) => {
+    await execute('rm -rf ./compose/src');
+    await execute(`curl ${tarUrl} | tar -xvz - -C ./compose/src`)
 }
 
 module.exports = {
